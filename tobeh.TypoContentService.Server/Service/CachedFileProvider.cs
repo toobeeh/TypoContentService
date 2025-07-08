b90ddc4d9b6ca8497906085ecad6b2503c000545
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-07-08 12:42:43 +0200

    use image download/upload proxy to avoid hetzner ip block

--- tobeh.TypoContentService.Server/Service/CachedFileProvider.cs
+++ tobeh.TypoContentService.Server/Service/CachedFileProvider.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json;
 using Microsoft.Extensions.Options;
 using tobeh.TypoContentService.Server.Config;
 
@@ -9,6 +10,9 @@ public class CachedFileProvider
     private readonly ImageGeneratorConfig _config;
     private readonly HttpClient _httpClient;
     private readonly ILogger<CachedFileProvider> _logger;
+    
+    private const string ProxyApiKey = "6d207e02198a847aa98d0a2a901485a5";
+    private const string ProxyUploadUrl = "https://freeimage.host/api/1/upload";
     
     private string GetCachePath(string base64Path) => Path.Combine(_config.CachePath, base64Path);
     
@@ -34,9 +38,39 @@ public class CachedFileProvider
         }
 
         _logger.LogDebug("Fetching and caching file from url {url} from cache", url);
-        var bytes = await _httpClient.GetByteArrayAsync(url);
+        
+        // workaround for hetzner server, where imgur blocks requests
+        // if not cached locally: upload to freeimghost via provided imgur url, and then download from freeimghost (de facto proxy)
+
+        var bytes = await ProxyDownloadViaFreeImageHost(url);
         
         await File.WriteAllBytesAsync(cachePath, bytes);
         return bytes;
+    }
+
+    private async Task<byte[]> ProxyDownloadViaFreeImageHost(string imgurUrl)
+    {
+        _logger.LogTrace("ProxyDownloadViaFreeImageHost(imgurUrl={imgurUrl})", imgurUrl);
+        
+        var form = new MultipartFormDataContent();
+        form.Add(new StringContent(ProxyApiKey), "key");
+        form.Add(new StringContent(imgurUrl), "source");
+        form.Add(new StringContent("json"), "format");
+        form.Add(new StringContent("upload"), "action");
+        
+        var response = await _httpClient.PostAsync(ProxyUploadUrl, form);
+        var json = await response.Content.ReadAsStringAsync();
+        _logger.LogDebug("Received response from freeimage.host: {json}", json);
+        
+        using var doc = JsonDocument.Parse(json);
+        var root = doc.RootElement;
+
+        var imageUrl = root
+            .GetProperty("image")
+            .GetProperty("url")
+            .GetString();
+        
+        byte[] imageBytes = await _httpClient.GetByteArrayAsync(imageUrl);
+        return imageBytes;
     }
 }
